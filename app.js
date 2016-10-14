93d210ffd9e682a254c006b23879297c37cc1250
amcavinue <aaronmcavinue@gmail.com>
2016-10-13 22:34:27 -0400

    Fin challenge.

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 // your code here!
+$(function() {
+	$('form').submit(function(e) {
+		e.preventDefault();
+		var text = $('#user-text').val(),
+			textSplit = text.split(/[^a-zA-Z0-9']+/ig);
+
+		$('.text-report dd').empty();
+
+		$('.js-total-word-count').append(textSplit.length);
+		$('.js-unique-word-count').append(uniqueWordCount(textSplit));
+		$('.js-average-word-length').append(averageWordLength(textSplit));
+		$('.js-average-sentence-length').append(averageSentenceLength(text));
+
+		$('.text-report').removeClass('hidden');
+	});
+});
+function uniqueWordCount(textSplit) {
+	return textSplit.filter(function(v, i, a) {
+		return a.indexOf(v) === i;
+	}).length;
+}
+
+function averageWordLength(textSplit) {
+	var numChars = 0;
+
+	for (var i = 0; i < textSplit.length; i++) {
+		numChars += textSplit[i].length;
+	}
+
+	return Math.floor(numChars / textSplit.length);
+}
+
+function averageSentenceLength(text) {
+	var textMatch = text.match(/[^\s][^\.!\?\n]+[\.!\?(?:\s)+]/ig),
+		wordCount = 0;
+
+	console.log(textMatch);
+
+	for (var i = 0; i < textMatch.length; i++) {
+		var sentenceSplit = textMatch[i].split(/[^a-zA-Z0-9']+/ig);
+
+		wordCount += sentenceSplit.length;
+	}
+
+	return Math.floor(wordCount / textMatch.length);
+}
